4a6a618fb72cd1e8ae080bb674e0a664c82c4cd0
sakilbd <mdsakiluzzaman@gmail.com>
2023-02-13 17:13:54 +0600

    some social icons added

--- src/Components/header/HeaderSocials.js
+++ src/Components/header/HeaderSocials.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import {BsLinkedin} from 'react-icons/bs'
-import {BsGithub} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs';
+import {BsGithub} from 'react-icons/bs';
+import {SiLeetcode} from 'react-icons/si';
+import {IoLogoCss3} from 'react-icons/io'
+
+
 
 class HeaderSocials extends Component {
     state = {  } 
     render() { 
         return (
             <div className="header__socials">
-                <a href="https://linkedin.com"><BsLinkedin/></a> 
-                <a href="https://github.com/sakilbd"><BsGithub/></a>    
-                   
-   
+                <a href="https://github.com/sakilbd" target="_blank"><SiLeetcode/></a>  
+                <a href="https://cssbattle.dev/player/sakilbd" target="_blank"><IoLogoCss3/></a>  
+                <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a> 
+                <a href="https://github.com/sakilbd" target="_blank"><BsGithub/></a>  
             </div>
         );
     }
