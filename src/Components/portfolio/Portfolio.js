3b969dbcce2f37a885a8808de344f5db62ba5ed3
unknown <mdsakiluzzaman@gmail.com>
2023-02-14 21:28:51 +0600

    some updates

--- src/Components/portfolio/Portfolio.js
+++ src/Components/portfolio/Portfolio.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import IMG1 from'../../Assets/linkedin_clone.PNG';
-import peripheralGif from '../../Assets/peripheral.gif'
+import peripheralGif from '../../Assets/peripheral.gif';
+import VEHICLE from '../../Assets/vehicle.PNG';
+import PIMS from '../../Assets/PIMS.PNG';
 import './portfolio.css';
 
 class Portfolio extends Component {
@@ -11,7 +13,7 @@ class Portfolio extends Component {
             <h5>My Recent works</h5>
             <h2>Portfolio</h2>
             <div className="container portfolio__container">
-                <article className="portfolio__item">
+                {/* <article className="portfolio__item">
                     <div className="portfolio__item-image">
                         <img src={IMG1} alt=""></img>
                     </div>
@@ -20,35 +22,45 @@ class Portfolio extends Component {
                             <a href="https://github.com" className="button" target='_blank'>Github</a>
                             <a href="https://github.com" className="button button-primary">Live Demo</a>
                         </div>
-                </article>
+                </article> */}
                 <article className="portfolio__item">
                     <div className="portfolio__item-image">
                         <img src={peripheralGif} alt=""></img>
                     </div>
                         <h3>Sensometer</h3>
                         <div className="portfolio__item-cta">
-                            <a href="https://github.com" className="button" target='_blank'>Github</a>
-                            <a href="https://github.com" className="button button-primary">Live Demo</a>
+                            {/* <a href="https://github.com" className="button" target='_blank'>Github</a> */}
+                            <a href="https://sensometer.mdsakiluzzaman.com" target='_blank' className="button button-primary">Live Demo</a>
                         </div>
                 </article>
                 <article className="portfolio__item">
                     <div className="portfolio__item-image">
-                        <img src={IMG1} alt=""></img>
+                        <img src={VEHICLE} alt=""></img>
                     </div>
-                        <h3>This is a portfolio item title</h3>
+                        <h3>Vehicle Tracking System</h3>
                         <div className="portfolio__item-cta">
-                            <a href="https://github.com" className="button" target='_blank'>Github</a>
-                            <a href="https://github.com" className="button button-primary">Live Demo</a>
+                            {/* <a href="https://github.com" className="button"  >Github</a> */}
+                            <a href="https://vehicle.mdsakiluzzaman.com" className="button button-primary" target='_blank'>Live Demo</a>
                         </div>
                 </article>
                 <article className="portfolio__item">
                     <div className="portfolio__item-image">
-                        <img src={IMG1} alt=""></img>
+                        <img src={PIMS} alt=""></img>
                     </div>
-                        <h3>This is a portfolio item title</h3>
+                        <h3>Personal Information Management System</h3>
                         <div className="portfolio__item-cta">
-                            <a href="https://github.com" className="button" target='_blank'>Github</a>
-                            <a href="https://github.com" className="button button-primary">Live Demo</a>
+                            {/* <a href="https://github.com" className="button" target='_blank'>Github</a> */}
+                            {/* <a href="https://github.com" className="button button-primary" target='_blank'>Live Demo</a> */}
+                        </div>
+                </article>
+                <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                        <img src={PIMS} alt=""></img>
+                    </div>
+                        <h3>Asset Tracking System</h3>
+                        <div className="portfolio__item-cta">
+                            {/* <a href="https://github.com" className="button" target='_blank'>Github</a> */}
+                            {/* <a href="https://github.com" className="button button-primary" target='_blank'>Live Demo</a> */}
                         </div>
                 </article>
                 
