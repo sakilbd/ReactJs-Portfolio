66ab03cc66f33646ad1d7495ea0644676889dd05
unknown <mdsakiluzzaman@gmail.com>
2023-02-16 21:12:22 +0600

    linked in link added

--- src/Components/header/HeaderSocials.js
+++ src/Components/header/HeaderSocials.js
@@ -13,7 +13,7 @@ class HeaderSocials extends Component {
             <div className="header__socials">
                 <a href="https://github.com/sakilbd" target="_blank"><SiLeetcode/></a>  
                 <a href="https://cssbattle.dev/player/sakilbd" target="_blank"><IoLogoCss3/></a>  
-                <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a> 
+                <a href="https://www.linkedin.com/in/mdsakil/" target="_blank"><BsLinkedin/></a> 
                 <a href="https://github.com/sakilbd" target="_blank"><BsGithub/></a>  
             </div>
         );
